8dfa62c85bcc80fbf89cb0b2bf99497feb146d35
MaksimSmolenkov <125339609+MaksimSmolenkov@users.noreply.github.com>
2025-01-22 19:46:01 +0600

    Update requests.sql

--- requests.sql
+++ requests.sql
@@ -51,3 +51,64 @@ SELECT FM.Name AS FacultyName, C.Name AS CourseName,
 FROM FacultyMember FM
 JOIN Course C ON FM.FacultyMemberID = C.FacultyMemberID;
 --Этот запрос использует оконную функцию ROW_NUMBER(), чтобы нумеровать курсы, которые преподает каждый преподаватель, в рамках каждого курса
+
+
+
+--ЗАПРОСЫ С ПОДЗАПРОСАМИ--
+SELECT Name, Program, GPA
+FROM Student
+WHERE GPA > (
+    SELECT AVG(GPA)
+    FROM Student S2
+    WHERE S2.Program = Student.Program
+);
+--Этот запрос использует подзапрос для вычисления среднего GPA для каждой программы и затем выбирает студентов, чьи GPA выше этого значения.
+
+SELECT Name
+FROM Course
+WHERE FacultyMemberID = (
+    SELECT FacultyMemberID
+    FROM (SELECT FacultyMemberID, COUNT(*) AS CourseCount
+          FROM Course
+          GROUP BY FacultyMemberID
+          ORDER BY CourseCount DESC
+          LIMIT 1) AS TopFaculty
+);
+--Этот запрос сначала использует подзапрос для получения преподавателя с максимальным количеством курсов, а затем извлекает курсы этого преподавателя.
+
+SELECT Name
+FROM Student
+WHERE StudentID IN (
+    SELECT DISTINCT S.StudentID
+    FROM Student S
+    JOIN Schedule Sch ON S.StudentID = Sch.StudentID
+    WHERE Sch.Room = '101'
+);
+--Этот запрос использует подзапрос для выбора студентов, которые посещают курсы в аудитории "101".
+
+
+
+--ЗАПРОСЫ С МАТЕРИАЛИЗАЦИЕЙ--
+CREATE MATERIALIZED VIEW AvgGPAByProgram AS
+SELECT Program, AVG(GPA) AS AvgGPA
+FROM Student
+GROUP BY Program;
+
+SELECT Program, AvgGPA
+FROM AvgGPAByProgram
+WHERE AvgGPA > 4.0;
+--Этот запрос извлекает программы, где средний GPA больше 4.0 из уже вычисленного и сохраненного в AvgGPAByProgram материала.
+
+CREATE MATERIALIZED VIEW CourseEnrollment AS
+SELECT C.CourseID, C.Name AS CourseName, COUNT(S.StudentID) AS EnrollmentCount
+FROM Course C
+LEFT JOIN Schedule Sch ON C.CourseID = Sch.CourseID
+LEFT JOIN Student S ON Sch.StudentID = S.StudentID
+GROUP BY C.CourseID, C.Name;
+
+SELECT CourseName, EnrollmentCount
+FROM CourseEnrollment
+WHERE EnrollmentCount > 50;
+--Этот запрос выбирает курсы, на которые записано больше 50 студентов, из материализованного представления CourseEnrollment.
+
+
